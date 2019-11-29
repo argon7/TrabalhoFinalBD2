d1d5242ae60cd0b5de50bbece7a266584bb364f0
João Aragão <emptechno@gmail.com>
2019-11-29 16:00:43 +0000

    --- fronted and routes

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db, login_manager
 from datetime import datetime
 from flask_login import UserMixin
 
+
 @login_manager.user_loader #decorator definiton
 def load_user(user_id):
     return User.query.get(int(user_id))
